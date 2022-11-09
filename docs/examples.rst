ccee5149975b7cbf42675fcc4ca85b5805c0a27d
Nathan Woody <wdyn1@mail.com>
2022-11-09 07:10:57 +0000

    project prep
    Using cookiecutter to make files

---
+++ docs/examples.rst
@@ -0,0 +1,8 @@
+Simple test
+------------
+
+Ensure your device works with this simple test.
+
+.. literalinclude:: ../examples/framework_simpletest.py
+    :caption: examples/framework_simpletest.py
+    :linenos:
---
+++ framework.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022 Nathan Woody
+#
+# SPDX-License-Identifier: MIT
+"""
+`framework`
+================================================================================
+
+Event driven multitasking for Circuitpython
+
+
+* Author(s): Nathan Woody
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s).
+  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
+  based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
+# imports
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/framework-circuitpython/CircuitPython_framework.git"
